8f2b014b144d1dbaf22cbd0ca7f4f065d00df428
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-04-01 02:51:23 +0200

    Final

---
+++ BankAccount.java
@@ -0,0 +1,122 @@
+
+
+import java.util.Random;
+
+	public class BankAccount {
+		
+	private static final String BIC = "ABCDDEFFXXX";
+	
+	private String Iban = "DE00123456780000000000";
+	
+	private int securityNumber;
+	
+	private Person owner;
+	
+	private double limit;
+	
+	private double balance;
+
+	public BankAccount(Person owner, double limit, double balance){
+		
+		this.owner=owner;
+		this.balance=balance;		
+	 this.limit=limit;
+			
+		
+		String a = "";
+		Random r = new Random();
+		
+		for(int i = 0; i<7; i++){
+			
+			a+=this.securityNumber= r.nextInt(10);	
+			
+			this.securityNumber = Integer.parseInt(a);
+		}
+
+		Iban = Iban.substring(0, 12);
+		
+		for(int i = 0; i<10; i++){
+			
+			Iban+=new Random().nextInt(10);
+		}								
+	}
+
+	public BankAccount(Person owner, double balance){
+		
+		this.owner=owner;
+		
+		this.balance=balance;	
+		
+		String a = "";
+		
+		Random r = new Random();
+		
+		for(int i = 0; i<6; i++){
+			
+			a+=this.securityNumber= r.nextInt(10);
+		}
+		
+		this.securityNumber = Integer.parseInt(a);	
+		
+		Iban = Iban.substring(0, 12);
+		
+		for(int i = 0; i<10; i++){
+			
+			Iban+=new Random().nextInt(10);
+			
+		}								
+	}
+
+	public double getbalance(){
+		return this.balance;
+	}
+	public void setbalance(double balance){
+		this.balance=balance;
+	}
+	public String getiban(){
+		return this.Iban;
+	}
+
+	public Person getowner(){
+		return this.owner;
+	}
+	
+	public int getSecurityNumber(){
+		return this.securityNumber;
+	}
+
+	public double getlimit(){
+		return this.limit;
+	}
+	
+
+	public double deposit(double amount, int securityNumber){		
+		if(securityNumber==this.securityNumber){
+			balance = balance + amount;
+			return balance;	
+		}
+		else {			
+
+		return balance;
+		}
+	}
+	public double withdraw(double amount, int securityNumber){		
+		if((securityNumber==this.securityNumber)&&(amount<=limit)){							
+		balance = balance - amount;
+		return balance;
+		}
+		else {			
+
+		return balance;
+		}
+	}
+
+	public void transfer(BankAccount remote, double amount, int securityNumber){
+		if((securityNumber==this.securityNumber)&&(amount<=limit)){							
+			 remote.setbalance(remote.getbalance()+amount);
+			 setbalance(balance - amount);
+		}else
+			System.out.println("Error");
+	}	
+
+} 
--- TestSuite.java
+++ TestSuite.java
@@ -3,13 +3,164 @@ import static TinyTestJ.Assert.*;
 
 public class TestSuite {
 
-  /*Initialize*/
-  //Test owner
-  @Test public static void InitTest1() {
-    //define
-    Person owner = new Person("John Doe");
-    BankAccount account = new BankAccount(owner, 1000.00);
-    //test
-    assertEquals(owner,account.getOwner(),0.001);
-  }
-}
+	@Test public static void Test1() {
+	     
+		Person owner = new Person("malik");
+	    BankAccount account = new BankAccount(owner, 3000.00,1500.00);
+	   
+	    assertEquals(1500.00,account.getlimit(),0.001);
+		}
+
+	@Test public static void Test2() {
+	     
+	    Person owner = new Person("malik");
+	    BankAccount account = new BankAccount(owner, 5000.00);
+	   
+	    assertEquals(0.00,account.getlimit(),0.001);
+		}
+
+	@Test public static void Test3() {
+	     
+	    Person owner = new Person("malik");
+	    BankAccount account = new BankAccount(owner, 6000.00);
+	    
+	    account.deposit(600.00,account.getSecurityNumber());
+	    account.withdraw(400.00,account.getSecurityNumber());
+	    account.deposit(500.00,account.getSecurityNumber());
+	    account.deposit(200.00,account.getSecurityNumber());
+	   
+	    assertEquals(900.00,account.getbalance(),0.001);
+		}
+
+	@Test public static void Test4() {
+	     
+	    Person owner = new Person("malik");
+	    BankAccount account = new BankAccount(owner, 3000.00,500.00);
+	    
+	      account.withdraw(700.00,account.getSecurityNumber());
+	   
+	    assertEquals(300.00,account.getbalance(),0.001);
+		}
+
+	@Test public static void Test5() {
+	     
+	    Person owner = new Person("malik");
+	    BankAccount account = new BankAccount(owner, 3000.00);
+	    System.out.println(account.getiban());
+	   
+	    assertEquals(22,account.getiban().length(),0.001);
+	}
+
+	@Test public static void Test6() {
+		 
+		Person owner = new Person("malik");
+		Person holder= new Person("shareef");
+		BankAccount account = new BankAccount(owner, 3000.00,2000.00);
+		BankAccount privateacc= new BankAccount(holder,5000.00,3000.00);
+		//changes
+		account.transfer(privateacc,400.00,account.getSecurityNumber());
+		account.deposit(800.00,account.getSecurityNumber());
+		//test
+		assertEquals(3400.00,account.getbalance(),0.001);
+	}
+
+	 @Test public static void Test7() {
+		
+		 Person owner = new Person("John Doe");
+		    BankAccount account = new BankAccount(owner, 1000.00,2000.00);
+		    //test
+		    assertEquals(2000.00,account.getlimit(),0.001);
+	  }
+
+	 @Test public static void Test8() {
+		   
+		  Person owner = new Person("malik");
+		  BankAccount account = new BankAccount(owner, 3000.00,2000.00);
+		  
+		  account.withdraw(4300.00,account.getSecurityNumber());
+		 
+		  assertEquals(-1300,account.getbalance(),0.001);
+	  }
+
+	 @Test public static void Test9() {
+		   
+		  Person owner = new Person("malik");
+		  Person newOwner=new Person("shareef");
+		  BankAccount account = new BankAccount(owner, 3000.00,500.00);
+		  BankAccount remote=new BankAccount(newOwner,3500.00);
+		  
+		    account.transfer(remote,300.00,account.getSecurityNumber());
+		 
+		  assertEquals(700.00,account.getbalance(),0.001);
+	  }
+
+
+	 @Test public static void Test10() {
+		   
+		  Person owner = new Person("malik");
+		  BankAccount account = new BankAccount(owner, 3000.00);
+		  boolean result = (account.getSecurityNumber()<1000000) && (account.getSecurityNumber()>10000);
+		 
+		  assertEquals(true,result,0.001);
+	  }
+
+	@Test public static void Test11() {
+		   
+		  Person owner = new Person("malik");
+		  BankAccount account = new BankAccount(owner, 3000.00);
+		 
+		  assertEquals(3000.00,account.getbalance(),0.001);
+	  }
+
+	   @Test public static void Test12() {
+		  
+		  Person owner = new Person("malik");
+		  BankAccount account = new BankAccount(owner, 3000.00,5000.00);
+	
+		  assertEquals(5000.00,account.getlimit(),0.001);
+	  }
+
+	   @Test public static void Test13() {
+		  
+		  Person owner = new Person("malik");
+		  Person newOwner=new Person("shareef");
+		  BankAccount account = new BankAccount(owner, 3000.00,500.00);
+		  BankAccount remote=new BankAccount(newOwner,3500.00);
+		  
+		    account.transfer(remote,300.00,account.getSecurityNumber());
+		 
+		  assertEquals(3800.00,remote.getbalance(),0.001);
+	  }
+
+	   @Test public static void Test14() {
+		   
+		  Person owner = new Person("malik");
+		  
+		  Person holder= new Person("shareef");
+		  
+		  BankAccount account = new BankAccount(owner, 1000.00,2000.00); 
+		  
+		  BankAccount privateacc= new BankAccount(holder,5000.00,1000.00);
+		  
+		  
+		  account.transfer(privateacc,600.00,account.getSecurityNumber());
+		  account.deposit(800.00,account.getSecurityNumber());
+		  
+		  
+		 
+		  assertEquals(5600.00,privateacc.getbalance(),0.001);
+	  }
+
+	  @Test public static void Test15() {
+		  Person owner = new Person("John Doe");
+		    BankAccount account = new BankAccount(owner, 2000.00);
+		    //changes
+		    account.deposit(700.00,account.getSecurityNumber());
+		    account.withdraw(1200.00,account.getSecurityNumber());
+		    account.deposit(500.00,account.getSecurityNumber());
+		    account.deposit(2100.00,account.getSecurityNumber());
+		    //test
+		    assertEquals(owner,account.getowner(),0.001);
+		    assertEquals(4100.00,account.getbalance(),0.001);
+		  }
+		}
